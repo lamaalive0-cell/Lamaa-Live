97a628203d0fb14fa558aa3c1a6219fc49374f51
lamaalive0-cell <lamaalive0@gmail.com>
2026-09-04 06:18:25 +0000

    ربط Supabase وتسجيل الدخول الحقيقي

---
+++ lib/core/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String url = 'https://fpwosplqsbnirjoleqaw.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwd29zcGxxc2JuaXJqb2xlcWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0OTgyMDksImV4cCI6MjEwNDA3NDIwOX0.jnqNZIauBqUmQ-tyvdx3lKdhmLHTHj9wITd_01puIKw';
+}
