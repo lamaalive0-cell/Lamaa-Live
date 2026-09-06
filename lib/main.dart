569f3d98d1a59f276cdf9f24a8dd9702237c9be4
lamaalive0-cell <lamaalive0@gmail.com>
2026-09-06 19:29:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'room_screen.dart'; // هذا السطر يستدعي ملف الغرفة الذي أنشأناه
+import 'room_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // اتصال سوبابيز الخاص بمشروعك
   await Supabase.initialize(
     url: 'https://fpwosplqsbnirjoleqaw.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwd29zcGxxc2JuaXJqb2xlcWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0OTgyMDksImV4cCI6MjEwNDA3NDIwOX0.jnqNZIauBqUmQ-tyvdx3lKdhmLHTHj9wITd_01puIKw',
@@ -23,9 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Lamaa Live',
       theme: ThemeData.dark(),
-      
-      // هنا جعلنا التطبيق يفتح مباشرة على "شاشة الغرفة"
-      home: const RoomScreen(roomId: 2), 
+      home: const RoomScreen(roomId: 2),
     );
   }
 }
